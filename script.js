da68003f9bcb4721b9c0a0f9238dda1c27ca9d05
Srivarshini Srinivasan <srivarshini.s847@gmail.com>
2026-01-22 12:09:42 +0530

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-body {
-    font-family: Arial, sans-serif;
-    text-align: center;
-    background-color: white;
-}
-
-button {
-    padding: 10px 20px;
-    margin: 10px;
-    background-color: #1a73e8;
-    color: red;
-    border: black;
-    border-radius: 5px;
-}
 
 console.log("CivicSphere loaded successfully");
 
