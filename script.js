2105f827c65792226b8d8d0ad053ba8b89ddf29d
Srivarshini Srinivasan <srivarshini.s847@gmail.com>
2026-01-22 12:06:30 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background-color: white;
+}
+
+button {
+    padding: 10px 20px;
+    margin: 10px;
+    background-color: #1a73e8;
+    color: red;
+    border: black;
+    border-radius: 5px;
+}
 
